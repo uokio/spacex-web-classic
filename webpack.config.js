24412f136d0f9c059fde2548aced39d844fd4611
于明龙 <yuminglong@gmail.com>
2017-08-23 19:40:28 +0800

    配置文件使用standard标准

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,89 @@
-  const path = require('path')
-  // 新引入
-  const webpack = require('webpack')
-  const HtmlWebpackPlugin = require('html-webpack-plugin');
-  const ExtractTextPlugin = require("extract-text-webpack-plugin")
-  const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin')
-  const CopyWebpackPlugin = require('copy-webpack-plugin')
-  const CleanWebpackPlugin = require('clean-webpack-plugin')
-  const glob = require('glob')
+const path = require('path')
+// 新引入
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const glob = require('glob')
 
-
-//文件查找
-  const jsFiles = glob.sync('./src/views/*/*.js')
-  const entryList = {}
-  const pluginList = []
-  for (const jsFile of jsFiles) {
-    const entryName = jsFile.slice('./src/views/'.length, -'.js'.length)
-    const entryPath = './'+jsFile.slice('./src/'.length)
-    const htmlName = entryName+".html"
-    const templatePath = './'+jsFile.slice('./src/'.length, -'.js'.length)+".html"
-    console.log(entryName+":"+entryPath)
-    console.log(htmlName+":"+templatePath)
-    entryList[entryName] = entryPath
-    pluginList.push(new HtmlWebpackPlugin({
-     chunks: ['common',entryName],
-     filename: htmlName,
-     template: templatePath,
-   }))
-  }
-
+// 文件查找
+const jsFiles = glob.sync('./src/views/*/*.js')
+const entryList = {}
+const pluginList = []
+for (const jsFile of jsFiles) {
+  const entryName = jsFile.slice('./src/views/'.length, -'.js'.length)
+  const entryPath = './' + jsFile.slice('./src/'.length)
+  const htmlName = entryName + '.html'
+  const templatePath = './' + jsFile.slice('./src/'.length, -'.js'.length) + '.html'
+  console.log(entryName + ':' + entryPath)
+  console.log(htmlName + ':' + templatePath)
+  entryList[entryName] = entryPath
+  pluginList.push(new HtmlWebpackPlugin({
+    chunks: ['common', entryName],
+    filename: htmlName,
+    template: templatePath
+  }))
+}
 
 module.exports = {
-    context: __dirname+"/src",
-    entry: 
-    Object.assign({
-      //'vendor': ["jquery","bootstrap"],
-      //'common': ['jquery'],
-    }, entryList),
-output: {
-  filename: 'js/[name].[hash].js',
-  path: __dirname + '/dist'
-},
+  context: path.resolve('./src'),
+  entry: Object.assign({
+    // 'vendor': ["jquery","bootstrap"],
+    // 'common': ['jquery'],
+  }, entryList),
+  output: {
+    filename: 'js/[name].[hash].js',
+    path: path.resolve('./dist')
+  },
 
-module: {
-  rules: [
+  module: {
+    rules: [
       // {test: /\.jpg$/, loader: "file-loader"},
       // {test: /\.png$/, loader: "url-loader?mimetype=image/png"},
-      {test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,use: 'file-loader'},
-      {test: /\.css$/, use: ExtractTextPlugin.extract("style-loader","css-loader")},
-      {test: /\.scss$/, use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: ['css-loader', 'sass-loader']
-      })},
+      {test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/, use: 'file-loader'},
+      {test: /\.css$/, use: ExtractTextPlugin.extract('style-loader', 'css-loader')},
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
       {
         enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        use:[
-        {loader: 'babel-loader',
-        options: {
-          presets: [['env', {modules: false}]],
-        }},
-        {loader: 'eslint-loader',
-        options: {
-          cache: true,
-          formatter: require('eslint-friendly-formatter')
-        }},
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['env', {modules: false}]]
+            }
+          },
+          {
+            loader: 'eslint-loader',
+            options: {
+              cache: true,
+              formatter: require('eslint-friendly-formatter')
+            }
+          }
         ]
-        
-      },
-      ],
-    },
-    externals: { 
-      jquery: "jQuery" 
-    },
-    resolve: { 
-      alias: { 
-       // jquery: path.resolve(__dirname, "src/libs/jquery/jquery-1.12.4.min.js" ),
-       // bootstrap: path.resolve(__dirname, "src/libs/bootstrap/js/bootstrap.min.js" )
-     } 
-   },
-   plugins: [
+
+      }
+    ]
+  },
+  externals: {
+    jquery: 'jQuery'
+  },
+  resolve: {
+    alias: {
+      // jquery: path.resolve(__dirname, "src/libs/jquery/jquery-1.12.4.min.js" ),
+      // bootstrap: path.resolve(__dirname, "src/libs/bootstrap/js/bootstrap.min.js" )
+    }
+  },
+  plugins: [
     // new webpack.ProvidePlugin({
     //     $: "jquery",
     //     jQuery: "jquery",
@@ -87,21 +91,22 @@ module: {
     // }),
     new CleanWebpackPlugin('dist'),
     new CopyWebpackPlugin([
-      { from: 'libs', to: 'libs/'},
-      ]),
-   //  new HtmlWebpackPlugin({
-   //   chunks: ['common','demo/page1'],
-   //   filename: 'demo/page1.html',
-   //   template: './views/demo/page1.html',
-   // }),
-   ...pluginList,
+      {from: 'libs', to: 'libs/'}
+    ]),
+    //  new HtmlWebpackPlugin({
+    //   chunks: ['common','demo/page1'],
+    //   filename: 'demo/page1.html',
+    //   template: './views/demo/page1.html',
+    // }),
+    ...pluginList,
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'common'  // 指代index.js引入的lodash库
+      // 指代index.js引入的lodash库
+      name: 'common'
     }),
     new ExtractTextPlugin('css/[name].[hash].css'),
     new HtmlWebpackIncludeAssetsPlugin({
-      assets: ['libs/jquery/jquery-1.12.4.min.js','libs/bootstrap/js/bootstrap.min.js','libs/bootstrap/css/bootstrap.min.css', 'libs/bootstrap/css/bootstrap-theme.min.css'],
+      assets: ['libs/jquery/jquery-1.12.4.min.js', 'libs/bootstrap/js/bootstrap.min.js', 'libs/bootstrap/css/bootstrap.min.css', 'libs/bootstrap/css/bootstrap-theme.min.css'],
       append: false
-    }),
-    ]
-  };
+    })
+  ]
+}
